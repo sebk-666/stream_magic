008912af9ef8e94f763f5e23deaa3cb92def3965
sebk-666 <seb.kaps@zoho.com>
2018-04-30 14:54:25 +0200

    Updated README

--- sync.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-rsync -e ssh -auz ~/Documents/Code/Python/cambridgeaudio/cambridgeaudio/cambridgeaudio.py root@dantana.sebastian-kaps.de:/home/seb/.homeassistant/custom_components/media_player/
-rsync -e ssh -auz ~/Documents/Code/Python/cambridgeaudio/cambridgeaudio/dmc.py root@dantana.sebastian-kaps.de:/home/seb/.homeassistant/python/
